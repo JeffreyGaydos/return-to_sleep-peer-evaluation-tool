41dc8c3ff2a827379d8585fe652a127bf1c5ad32
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-11-24 16:52:10 -0500

    frontend for reset password screen

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,4 +2,8 @@ class LoginController < ApplicationController
   def init
     render(:layout => 'authentication.html.erb')
   end
+
+  def reset_password
+    render(:layout => 'authentication.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/login', to: 'login#init'
+  get '/reset_password', to: 'login#reset_password'
   get '/', to: 'home_page#init'
+  get '/root', as: '/', to: 'home_page#init'
 end
