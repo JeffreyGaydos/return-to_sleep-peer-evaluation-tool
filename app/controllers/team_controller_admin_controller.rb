de4ecb6ce21ff850663eef59abdbf5131da9b27c
Jeremy Yao <yao.789@osu.edu>
2021-11-29 14:14:00 -0500

    barebones team controllers methods

--- app/controllers/team_controller_admin_controller.rb
+++ app/controllers/team_controller_admin_controller.rb
@@ -1,4 +1,6 @@
 class TeamControllerAdminController < ApplicationController
+
+  # Renders the team_listing page for a specific course as an admin.
   def team_list
     @admin_current_course = "Insert current course for admin here"
     # @admin_team_listings - Team...
@@ -12,11 +14,28 @@ class TeamControllerAdminController < ApplicationController
     team_list
   end
 
-  def team_create
+  # Edit a team by going to its view.
+  def team_edit
+
   end
 
-  # View specific team.
+  # Renders the team_create page.
+  def team_create
+
+  end
+
+  # Go to here once we clicked the submit/enter button on the team_create page.
+  def team_create_submit
+
+  end
+
+  # Renders a spage where we view specific team and give options to edit team, like kicking a student
   def team_view
+
+  end
+
+  # Kicks a student from a team.
+  def kick_student_from_team
 
   end
 end
