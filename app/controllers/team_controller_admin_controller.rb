694d58431fb5b891cf69b32dbe962a22c388e557
Jeremy Yao <yao.789@osu.edu>
2021-11-29 14:02:05 -0500

    slight modifications to team controllers and team_list.

--- app/controllers/team_controller_admin_controller.rb
+++ app/controllers/team_controller_admin_controller.rb
@@ -1,10 +1,22 @@
 class TeamControllerAdminController < ApplicationController
   def team_list
+    @admin_current_course = "Insert current course for admin here"
+    # @admin_team_listings - Team...
+    # Get an array of all of the teams asssociated with @admin_current_course
+    # Display all of them and given options for editing and deleting.
+  end
+
+  def team_delete
+    # Delete specific team from db.
+    # Re-render team_list page.
+    team_list
   end
 
   def team_create
   end
 
+  # View specific team.
   def team_view
+
   end
 end
