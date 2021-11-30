b683dbefc7c067ddacffa8adbf88d78d486a4a03
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-11-29 23:18:20 -0500

    fix false positive tests

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ApplicationRecord
     validates :class_number, presence: true, numericality: { greater_than: 0 }
     has_many :course_user
     has_many :users, through: :course_user
+
+    has_many :course_admin
+    has_many :admins, through: :course_admin
 end
--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -3,7 +3,14 @@ require 'test_helper'
 class AdminTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
-    @admin = Admin.new(user_id: 1, institution_id: 1)
+    @user.save
+    @institution = Institution.new(name_id: "OSU")
+    @institution.save
+    @admin = Admin.new(user: @user, user_id: 1, institution: @institution, institution_id: 1)
+  end
+
+  test "base case" do
+    assert @admin.valid?
   end
 
   test "admin must have 1 user" do
@@ -14,10 +21,6 @@ class AdminTest < ActiveSupport::TestCase
   test "admin must have 1 institution" do
     bad_admin = Admin.new(user_id: 1)
     assert_not bad_admin.valid?
-  end
-
-  test "admin must have a user" do
-    assert_not @admin.valid?
   end
 
   test "admins cannot share 1 user" do
