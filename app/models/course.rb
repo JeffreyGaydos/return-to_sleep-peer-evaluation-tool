d40edd306f61cb69d32594ec28843477dd239bef
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-11-29 22:33:44 -0500

    validations for course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
     validates :class_number, presence: true, numericality: { greater_than: 0 }
-    has_many :users_course
-    has_many :users, through: :users_course
+    has_many :course_user
+    has_many :users, through: :course_user
 end
