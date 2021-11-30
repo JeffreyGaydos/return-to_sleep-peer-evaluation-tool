669532b242a1332ff8af45025f95c26c42c4ce40
Will <babydoodles99@gmail.com>
2021-11-30 18:55:04 -0500

    changes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,15 @@ class CoursesController < ApplicationController
     def show
         @course = Course.find(params[:id])
         render(:layout => "application.html.erb")
+    end 
+
+    def index
+        @course = Course.all
+    end
+
+    def create
+        @course = Course.create(course_params)
+        redirect_to "/Course/new"
     end
 
 
