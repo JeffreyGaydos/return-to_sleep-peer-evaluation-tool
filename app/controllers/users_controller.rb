7d546d789c01db0ccdead8ace6c021af866e638a
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-11-30 22:04:03 -0500

    add associated teams to student account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
         @courses = @user.courses
+        @teams = @user.teams
         render(:layout => 'internal.html.erb')
     end
 
