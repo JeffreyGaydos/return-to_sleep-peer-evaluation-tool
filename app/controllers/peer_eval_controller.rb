62ccc31da652edc679af90858edb680b0c386113
Stephen Chou <sccusr@gmail.com>
2021-12-02 03:06:09 +0000

    Form submission working, need to figure out routes

--- app/controllers/peer_eval_controller.rb
+++ app/controllers/peer_eval_controller.rb
@@ -7,24 +7,27 @@ class PeerEvalController < ApplicationController
   def form_view
     
     @peer_eval = PeerEval.new
-    @users = User.all
+    @users = User.all.select { |user| user.id != current_user[:id] }
 
   end
 
   def create
     puts params
     @peer_eval = PeerEval.new
-    @peer_eval.score = params[:score]
-    @peer_eval.comment = params[:comment]
-    eval_user = User.find_by(id: params[:evaluated_user_id])
-    @peer_eval.user = params[:evaluated_user_id]
-        # if @peer_eval.save
-    # end
+    @peer_eval.score = params[:peer_eval][:score]
+    @peer_eval.comment = params[:peer_eval][:comment]
+    @peer_eval.user_id = params[:peer_eval][:evaluated_user_id]
+    @peer_eval.team_id = 1
+    @peer_eval.project_id = 1
+    
+    if @peer_eval.save!
+      redirect_to "/users/#{current_user[:id]}"
+    end
   end
   
   private
     def peer_eval_params
-      params.require(:peer_eval).permit!
+      params.require(:peer_eval).permit(:score, :comment, :user, :team, :project)
     end
 
 end
--- app/models/peer_eval.rb
+++ app/models/peer_eval.rb
@@ -1,7 +1,6 @@
 class PeerEval < ApplicationRecord
   validates :score, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :comment, presence: true, length: { maximum: 255 }
-  validates :evaluated_user, presence: true
 
   # define database relationships
   belongs_to :user
