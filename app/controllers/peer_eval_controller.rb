bc0de97ecd5c8621000a9b535e85fa2d61411ec2
Stephen Chou <sccusr@gmail.com>
2021-12-03 00:49:54 +0000

    Started working on peer evaluation index view

--- app/controllers/peer_eval_controller.rb
+++ app/controllers/peer_eval_controller.rb
@@ -5,6 +5,12 @@ class PeerEvalController < ApplicationController
   #   @users = User.all.select { |user| user.id != current_user[:id] }
   # end
 
+  def index
+    @peer_evals = PeerEval.all
+    @project_num = params[:project_id]
+    @users = User.all
+  end
+
   def new
     
     @peer_eval = PeerEval.new
@@ -13,15 +19,18 @@ class PeerEvalController < ApplicationController
   end
 
   def create
-    puts params
-    @peer_eval = PeerEval.new
-    @peer_eval.score = params[:peer_eval][:score]
-    @peer_eval.comment = params[:peer_eval][:comment]
-    @peer_eval.user_id = params[:peer_eval][:evaluated_user_id]
-    @peer_eval.team_id = 1
-    @peer_eval.project_id = 1
+
+    @peer_eval = PeerEval.new # create new peer eval object
+    @peer_eval.score = params[:peer_eval][:score] # add score from form
+    @peer_eval.comment = params[:peer_eval][:comment] # add comment from form
+    evaluated_user_id = params[:peer_eval][:evaluated_user_id]
+    @peer_eval.user_id =  evaluated_user_id # add evaluated user from dropdown
+    @peer_eval.team_id = params[:team_id] # add team id from url params
+    @peer_eval.project_id = params[:project_id] # add project id from url params
     
-    if @peer_eval.save!
+    if PeerEval.exists?(user_id: evaluated_user_id)
+      flash[:alert] = "ERROR: Peer evaluation already exists for this user"
+    elsif @peer_eval.save!
       redirect_to "/users/#{current_user[:id]}"
     end
   end
