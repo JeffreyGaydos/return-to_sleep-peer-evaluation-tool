5a336ba82c8045c3a22acb0921996dcbc7679af5
JeffreyGaydos <jeff13gaydos@gmail.com>
2021-12-06 17:11:13 -0500

    user/admin team and course relations for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,18 +26,18 @@
 
 # Admin Users, or Instructors:
 @instructor1 = User.create(name: "Instructor1", email: "instructor1@example.com", password: "Something", password_confirmation: "Something")
-@admin1 = Admin.new(user: @instructor1, user_id: @instructor1.id, institution: @institution1, institution_id: @institution1.id)
+@admin1 = Admin.create(user: @instructor1, user_id: @instructor1.id, institution: @institution1, institution_id: @institution1.id)
 @instructor2 = User.create(name: "Instructor2", email: "instructor2@example.com", password: "Something", password_confirmation: "Something")
-@admin2 = Admin.new(user: @instructor2, user_id: @instructor2.id, institution: @institution2, institution_id: @institution2.id)
+@admin2 = Admin.create(user: @instructor2, user_id: @instructor2.id, institution: @institution2, institution_id: @institution2.id)
 @instructor3 = User.create(name: "Instructor3", email: "instructor3@example.com", password: "Something", password_confirmation: "Something")
-@admin3 = Admin.new(user: @instructor3, user_id: @instructor3.id, institution: @institution3, institution_id: @institution3.id)
+@admin3 = Admin.create(user: @instructor3, user_id: @instructor3.id, institution: @institution3, institution_id: @institution3.id)
 
 # Courses
-@course1 = Course.new(name: "Web Apps 1", class_number: 3901)
-@course2 = Course.new(name: "Web Apps 2", class_number: 3902)
-@course3 = Course.new(name: "Web Apps 3", class_number: 3903)
-@course4 = Course.new(name: "Web Apps 4", class_number: 3904)
-@course5 = Course.new(name: "Web Apps 5", class_number: 3905)
+@course1 = Course.create(name: "Web Apps 1", class_number: 3901)
+@course2 = Course.create(name: "Web Apps 2", class_number: 3902)
+@course3 = Course.create(name: "Web Apps 3", class_number: 3903)
+@course4 = Course.create(name: "Web Apps 4", class_number: 3904)
+@course5 = Course.create(name: "Web Apps 5", class_number: 3905)
 
 # Teams
 @team1 = Team.create(name: "Team 1", course: @course1)
@@ -49,3 +49,56 @@
 @team7 = Team.create(name: "Team 7", course: @course4)
 @team8 = Team.create(name: "Team 8", course: @course5)
 
+# Projects
+@project1 = Project.create(name: "Game of Set", team: @team1)
+@project2 = Project.create(name: "Tech Presentation", team: @team1)
+@project3 = Project.create(name: "Rails Final", team: @team1)
+@project4 = Project.create(name: "Game of Set", team: @team2)
+@project5 = Project.create(name: "Game of Set in JS", team: @team2)
+@project6 = Project.create(name: "Game of Set", team: @team3)
+@project7 = Project.create(name: "Game of Set", team: @team4)
+@project8 = Project.create(name: "Game of Set", team: @team5)
+@project9 = Project.create(name: "Game of Set", team: @team6)
+@project10 = Project.create(name: "Game of Set", team: @team7)
+@project11 = Project.create(name: "Game of Set", team: @team8)
+
+#User-Course relations
+@user1.courses << @course1
+@user1.courses << @course2
+@user1.courses << @course5
+@user2.courses << @course1
+@user2.courses << @course2
+@user2.courses << @course4
+@user3.courses << @course1
+@user3.courses << @course3
+@user4.courses << @course1
+@user5.courses << @course2
+@user6.courses << @course3
+@user7.courses << @course4
+@user8.courses << @course5
+@user9.courses << @course5
+@user10.courses << @course5
+
+@admin1.courses << @course1
+@admin2.courses << @course1
+@admin1.courses << @course2
+@admin2.courses << @course3
+@admin3.courses << @course4
+@admin3.courses << @course5
+
+#User-team relations
+@team1.users << @user1
+@team1.users << @user2
+@team1.users << @user3
+@team1.users << @user4
+@team2.users << @user1
+@team2.users << @user2
+@team3.users << @user3
+@team3.users << @user5
+@team4.users << @user4
+@team4.users << @user6
+@team5.users << @user5
+@team5.users << @user7
+@team6.users << @user8
+@team7.users << @user9
+@team8.users << @user10
