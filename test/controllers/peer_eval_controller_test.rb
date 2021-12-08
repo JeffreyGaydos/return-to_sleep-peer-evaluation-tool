7f5d46ed4b42e9801bae5cb0b020bd27bbd3c3a5
Stephen Chou <sccusr@gmail.com>
2021-12-08 00:59:11 +0000

    Added test cases

--- test/controllers/peer_eval_controller_test.rb
+++ test/controllers/peer_eval_controller_test.rb
@@ -9,6 +9,7 @@ class PeerEvalControllerTest < ActionDispatch::IntegrationTest
     @course = Course.new(class_number: 3901, name:"web dev")
     @team = Team.new(name: "team name", course: @course)
     @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user2 = User.new(name: "Example User2", email: "user2@example.com", password: "foobar2", password_confirmation: "foobar2")
     @project = Project.new(name: "Project")
     # @peer_eval = PeerEval.new(score: 10, comment: "nice job")
 
@@ -16,6 +17,7 @@ class PeerEvalControllerTest < ActionDispatch::IntegrationTest
     @team.users << @user
     @team.projects << @project
     @project.team = @team
+    @team.users << @user2
 
     # @peer_eval.user = @user
     # @peer_eval.team = @team
@@ -24,12 +26,33 @@ class PeerEvalControllerTest < ActionDispatch::IntegrationTest
     @course.save
     @team.save
     @user.save
+    @user2.save
     @project.save
+    @base_title = "Peer Evaluation Tool - "
   end
 
   test "valid peer evaluation should be created" do
-    get new_team_project_peer_eval_path
-    post "/new/1", params: { peer_eval: {score: 10, comment: "nice work"} }
+
+    # login as user
+    get '/login'
+    assert_template 'login/init'
+    post '/login', params: { session: { email: "user@example.com", password: "foobar" } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_select "title", "#{@base_title}Account"
+    assert session[:user_id]
+
+    # POST peer eval
+    get "/teams/1/projects/1/peer_eval/new?evaluated_user_id=2"
+    
+    # assert that new user is created
+    assert_difference 'PeerEval.count', 1 do
+      post "/teams/1/projects/1/peer_eval", params: { peer_eval: {score: 10, comment: "nice work", user_id: 2, team_id: 1, project_id: 1} }
+    end
+
+      assert_response :redirect
+    follow_redirect!
     assert_response :success
   end
 
